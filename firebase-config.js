5a2596d1ba14a3236018a22b066794ec56abcb69
psgpgm-code <psgpgm@gmail.com>
2026-08-23 18:43:07 +0530

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -6,10 +6,6 @@ window.SKMED_FIREBASE_CONFIG = {
   messagingSenderId: "921893232974",
   appId: "1:921893232974:web:45813196e59052e9597e1f"
 };
-
-// Admin Portal authorized email பின்னர் இங்கே சேர்க்கலாம்.
 window.SKMED_ADMIN_EMAILS = [];
-
-// Optional UPI details
 window.SKMED_UPI_ID = "";
 window.SKMED_UPI_NAME = "Sri Krishna Medicals";
